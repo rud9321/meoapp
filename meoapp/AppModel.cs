2d793ae3f731dfd3d58b754657ce4d9ecc4b3fe1
rud9321 <rud9321@gmail.com>
2020-07-26 17:23:45 +0530

    first commit

---
+++ meoapp/AppModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace meoapp
+{
+    public class appModel
+    {
+        public DateTime Date { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
---
+++ meoapp/Controllers/WappController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace meoapp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WappController : ControllerBase
+    {
+        private static readonly string[] something = new[]
+        {
+            "city", "state", "cold", "winter", "nasha", "story", "my"
+        };
+
+        private readonly ILogger<WappController> _logger;
+
+        public WappController(ILogger<WappController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<appModel> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new appModel
+            {
+                Date = DateTime.Now.AddDays(index),
+                Summary = something[rng.Next(something.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
